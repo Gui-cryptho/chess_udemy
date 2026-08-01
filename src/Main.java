939333dca562bc4975b716c6f622b31e623fc2ea
Guilherme <guilhermesb095@gmail.com>
2026-08-01 11:03:28 -0300

    feat: boardLayer created and positon class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import boardLayer.Position;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
+        Position p1 = new Position(8, 4);
+
+        System.out.println(p1);
 
     }
 }
