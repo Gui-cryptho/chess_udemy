6f29efaa632ec2b3724acca0c25d75d46eebfe36
Guilherme <guilhermesb095@gmail.com>
2026-08-08 23:35:17 -0300

    feat: class Board and Piece created

---
+++ src/boardLayer/Piece.java
@@ -0,0 +1,19 @@
+package boardLayer;
+
+import boardLayer.auxType.Position;
+
+public class Piece {
+
+    private Board board;
+    protected Position position;
+
+    public Piece(Board board) {
+        this.board = board;
+        this.position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+}
